948fbaad833b99d84f174b3cc9cd5e9c64f279b8
Siyabonga Samson Lukhele <samsonlukhele76@gmail.com>
2025-05-01 19:30:49 +0200

    Last commit: Footer appearance refactor

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,18 +5,22 @@ const Footer = () => {
   const year = new Date().getFullYear();
 
   return (
-    <footer className="fixed bottom-0 left-0 w-full bg-gray-900  text-gray-600 text-sm flex items-center justify-center py-2 px-4 shadow-sm z-50">
-      <span className="mr-2">© {year} Samson Siyabonga Lukhele</span>
-      <a
-        href="https://github.com/markuptitan/react-portfolio"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="ml-2 text-gray-400 hover:text-blue-700 transition-colors"
-        aria-label="GitHub Repo"
-      >
-        <FaGithub size={18} />
-      </a>
-      <ThemeSwitch />
+    <footer className="fixed bottom-0 left-0 w-full bg-[var(--background)] text-[var(--foreground)] text-sm flex items-center justify-between py-2 px-4 shadow-inner z-50">
+      <span className="text-[var(--primary)] font-light">
+        © {year} Samson Siyabonga Lukhele
+      </span>
+      <div className="flex items-center">
+        <a
+          href="https://github.com/markuptitan/react-portfolio"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="ml-2 text-[var(--foreground)] hover:text-[var(--primary)] transition-colors"
+          aria-label="GitHub Repo"
+        >
+          <FaGithub size={18} />
+        </a>
+        <ThemeSwitch />
+      </div>
     </footer>
   );
 };
